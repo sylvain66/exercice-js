956bfdc17241aaf3eedf47c23b22029affdef2f6
sylvain05 <factory05@gmail.com>
2016-11-08 15:27:11 +0100

    base1

---
+++ exo6custom.js
@@ -0,0 +1,7 @@
+
+  function Valid(){
+      var premier_nombre = document.getElementById('premier_nombre').value;
+      var deuxieme_nombre =document.getElementById('deuxieme_nombre').value;
+      var result = premier_nombre % deuxieme_nombre;
+      alert(result);
+}
